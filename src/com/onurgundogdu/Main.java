6c4682d58064f66035af92118870d6476c030334
gundogduonur <gundogduonur@outlook.com.tr>
2022-04-29 14:35:17 +0300

    added  only once running thread

--- src/com/onurgundogdu/Main.java
+++ src/com/onurgundogdu/Main.java
@@ -6,33 +6,51 @@ import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
         // -----Anonymous Class-------
-           Thread printer1=new Thread(new Runnable() {
-               @Override
-               public void run() {
-                     System.out.println("Thread running");
-                   for (int i = 1; i <5 ; i++) {
-                       try
-                       {
-                           System.out.println("Printing: "+i);
-                           Thread.sleep(1000);
-                       }
-                       catch (InterruptedException interruptedException)
-                       {
-                           Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,interruptedException);
-                       }
-                   }
+        // Thread printer1=new Thread(new Runnable() {
+        //  @Override
+        // public void run() {
+        //     System.out.println("Thread running");
+        //  for (int i = 1; i <5 ; i++) {
+        //     try
+        //   {
+        //      System.out.println("Printing: "+i);
+        //     Thread.sleep(1000);
+        //   }
+        //    catch (InterruptedException interruptedException)
+        //     {
+        //       Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,interruptedException);
+        //     }
+        //     }
+        //     }
+        //     });
+        //     printer1.start();
+
+        // -----Only once running thread----
+        new Thread(new Runnable() {
+            //  @Override
+             public void run() {
+                 System.out.println("Thread running");
+              for (int i = 1; i <5 ; i++) {
+                 try
+               {
+                  System.out.println("Printing: "+i);
+                 Thread.sleep(1000);
                }
-           });
-           printer1.start();
-
-
+                catch (InterruptedException interruptedException)
+                 {
+                   Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,interruptedException);
+                 }
+              }
+                 }
+                 })
+                 .start();
         // ------Using implement Runnable------
         // System.out.println("Main thread running");
         // Thread printer1=new Thread(new Printer("Printer1"));
         // Thread printer2=new Thread(new Printer("Printer2"));
         // printer1.start();
         // printer2.start();
-        
+
         // ------Using extends Thread-----
         // Printer printer1=new Printer("Printer1");
         // Printer printer2=new Printer("Printer2");
