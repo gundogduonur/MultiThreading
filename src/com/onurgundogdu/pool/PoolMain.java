c970b2b8e53f276a09a602dbceb056a1ed98bc9a
gundogduonur <gundogduonur@outlook.com.tr>
2022-05-28 17:33:15 +0300

    run threads

--- src/com/onurgundogdu/pool/PoolMain.java
+++ src/com/onurgundogdu/pool/PoolMain.java
@@ -11,16 +11,18 @@ import java.util.logging.Logger;
 public class PoolMain {
     public static void main(String[] args) {
         {
-            ExecutorService executorService = Executors.newFixedThreadPool(3);
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
             for (int i = 1; i <=5 ; i++) {
                 executorService.submit(new Worker(String.valueOf(i),i));
             }
             executorService.shutdown();
+            System.out.println("Submit to Tasks");
             try {
                 executorService.awaitTermination(1, TimeUnit.DAYS); // wait at the most one day
             } catch (InterruptedException e) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,e);
             }
+            System.out.println("Successfully completed.");
         }
 
     }
