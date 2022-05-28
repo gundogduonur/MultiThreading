453dcba3a96e737d226140166c3b2e16c96661ba
gundogduonur <gundogduonur@outlook.com.tr>
2022-05-28 17:51:19 +0300

    created arrayblocking package and class

---
+++ src/com/onurgundogdu/arrayblocking/ProducerConsumer.java
@@ -0,0 +1,13 @@
+package com.onurgundogdu.arrayblocking;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerConsumer {
+
+    Random random= new Random();
+    BlockingQueue<Integer> queue=new ArrayBlockingQueue<>(10);
+
+   
+}
