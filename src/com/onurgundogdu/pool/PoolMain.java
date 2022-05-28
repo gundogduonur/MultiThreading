c59b45ecd01510265674d3fdc302af9b7189a165
gundogduonur <gundogduonur@outlook.com.tr>
2022-05-28 17:23:16 +0300

    ExecuterService

--- src/com/onurgundogdu/pool/PoolMain.java
+++ src/com/onurgundogdu/pool/PoolMain.java
@@ -1,28 +1,17 @@
 package com.onurgundogdu.pool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class PoolMain {
     public static void main(String[] args) {
-        Thread thread1=new Thread(new Worker("1",1));
-        Thread thread2=new Thread(new Worker("2",2));
-        Thread thread3=new Thread(new Worker("3",3));
-
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        {
+            ExecutorService executorService = Executors.newFixedThreadPool(3);
+            for (int i = 1; i <=5 ; i++) {
+                executorService.submit(new Worker(String.valueOf(i),i));
+            }
+            executorService.shutdown();
         }
-        thread3.start();
-
-        try {
-            thread3.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Successfully Completed..");
 
     }
 }
