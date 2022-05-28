d4ab8314f7ca4067bd7f515733fe4fcedcb91ce6
gundogduonur <gundogduonur@outlook.com.tr>
2022-05-28 18:02:00 +0300

    defined threads and run

--- src/com/onurgundogdu/arrayblocking/Main.java
+++ src/com/onurgundogdu/arrayblocking/Main.java
@@ -2,6 +2,18 @@ package com.onurgundogdu.arrayblocking;
 
 public class Main {
     public static void main(String[] args) {
-
+     ProducerConsumer producerConsumer=new ProducerConsumer();
+     Thread producer=new Thread(new Runnable() {
+         @Override
+         public void run() {
+             producerConsumer.produce();
+         }
+     });
+     Thread consumer=new Thread(new Runnable() {
+         @Override
+         public void run() {
+             producerConsumer.consume();
+         }
+     });
     }
 }
